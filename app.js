8a19543faebb35dd3593cbd1b3ea0d710efa4f38
ChristopherL <101825137+ChristopherL-creator@users.noreply.github.com>
2022-05-24 10:45:11 +0200

    es prof

--- app.js
+++ app.js
@@ -8,10 +8,14 @@ const createToDoDate = (name) => {
 } 
 
 const createToDoTags = (tags) => { 
-    const div = document.createElement('div'); 
-    div.classList.add('todo-tags');
-    div.appendChild(document.createTextNode(tags)); 
-    return div; 
+    const tagsContainer = document.createElement('div'); 
+    tagsContainer.classList.add('todo-tags'); 
+
+    const tag = document.createElement('div'); 
+    tag.classList.add('tags-items');
+    tag.appendChild(document.createTextNode(tags)); 
+    tagsContainer.appendChild(tag);  
+    return tagsContainer; 
 } 
 
 const createToDoName = (name) => { 
@@ -92,11 +96,12 @@ const displayToDos = (arrayOfToDos) => {
     headerH1.appendChild(titleNode); 
     toDoHeader.appendChild(headerH1); 
     
-    const menuButton = document.createElement('button'); 
-    menuButton.classList.add('menu-button');
-    const menuButtonNode = document.createTextNode('='); 
-    menuButton.appendChild(menuButtonNode); 
-    toDoHeader.appendChild(menuButton); 
+    const updateButton = document.createElement('button'); 
+    updateButton.classList.add('update-button'); 
+    updateButton.onclick = () => initApp(); 
+    const updateButtonNode = document.createTextNode('Update'); 
+    updateButton.appendChild(updateButtonNode); 
+    toDoHeader.appendChild(updateButton); 
     
     const arrayContainer = document.createElement('div');
     arrayContainer.classList.add('array-container');
@@ -121,4 +126,93 @@ const initApp = () => fetch("https://628b2f687886bbbb37b2139d.mockapi.io/todo")
                      .then(resultCallBack) 
                      .catch(catchError);
 
-initApp();
+initApp(); 
+
+//___________________________________________________________________________________________
+
+// const BASE_URL = 'https://628b2f687886bbbb37b2139d.mockapi.io/todo'; 
+
+// const todosArray = [];  
+
+// function startLoading() {
+//     const loader = document.getElementById('loader'); 
+//     loader.style.display = 'inline-block'; 
+    // const refresh = document.getElementById('refresh-button'); 
+    // refresh.style.display = 'none';
+// } 
+
+// function stoptLoading() {
+//     const loader = document.getElementById('loader'); 
+//     loader.style.display = 'none'; 
+    // const refresh = document.getElementById('refresh-button'); 
+    // refresh.style.display = 'inline';
+// }
+
+// function filtertoDos(t1, t2) {
+//     return t1.id !== t2.id;
+// } 
+// //  tiene tutti i display diversi dal todo originario;
+
+// function removeToDoAndRefresh(todo) {
+//     todosArray = todosArray.filter(t1 => filtertoDos(t1, todo));
+//     displayToDos(todosArray);
+// } 
+//  per ogni t1 nell'array, controlla che non sia uguale a t2;
+
+// function deleteToDo(id){ 
+//     startLoading()
+//     const deleteUrl = BASE_URL + '/' + ID; 
+//     const fetchOptions = { 
+//          method: 'delete'
+//     }; 
+        // fetch(deleteUrl, fetchOptions) 
+        // .then(response => response.json()) 
+        // .then(result => removeToDoAndRefresh(result)) 
+//         .catch(error => stopLoading())
+// }
+
+// function displayToDos(todos){  
+    
+    // const toDosContainer = document.getElementById('todos-container'); 
+
+    // toDosContainer.innerHTML= '';
+//     for (const todo of todos) {
+// //  div contenitore
+//         const todoCard = document.createElement('div');
+// //  creo span; ci metto quindi dentro node 
+//         const span = document.createElement('span'); 
+//         const nameNode = document.createTextNode(todo.name); 
+//         span.appendChild(nameNode); 
+//         todoCard.appendChild(span); 
+        
+//         const button = document.createElement('button'); 
+//         button.onclick = () => deleteToDo(todo.id); 
+//  la chiamo come lambda, sennò lo esegue subito; se non dovessi 
+//  passare nessun parametro, ci tolgo le tonde, sennò la esegue subito; 
+//  devo quindi creare funzione contenitore della funzione che non devo eseguire 
+//  subito (lambda);
+//         const deleteNode = document.createTextNode('delete'); 
+
+//         button.appendChild(deleteNode); 
+//         todoCard.appendChild(button);
+        
+//         toDosContainer.appendChild(todoCard); 
+
+//     } 
+
+// } 
+
+// function initToDos(todos){ 
+//     todosArray = todos; 
+//     displayToDos(todosArray);
+// }
+
+// function loadToDos() { 
+//     startLoading()
+//     fetch(BASE_URL) 
+//     .then(response => response.json) 
+//     .then(result => initToDos(result))
+//     .catch(error => stopLoading())
+// } 
+
+// loadToDos();
